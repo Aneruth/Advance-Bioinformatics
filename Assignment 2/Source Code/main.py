5e2b6675bcfe2d780e45b0a1138f5383b1639c8c
Aneruth Mohanasundaram <ane1998@gmail.com>
2021-04-20 16:43:40 +0200

    New function to fetch sequence and quality score

--- Assignment 2/Source Code/main.py
+++ Assignment 2/Source Code/main.py
@@ -1,21 +1,37 @@
+# Importing packages to visualise
+import matplotlib.pyplot as plt
+
 genome_info = '/Users/aneruthmohanasundaram/Documents/GitHub/Advance-Bioinformatics/Assignment 2/Datasets/dm6.fa'
 sequence_info = '/Users/aneruthmohanasundaram/Documents/GitHub/Advance-Bioinformatics/Assignment 2/Datasets/10k_reads.fastq'
 
-def fileRead(aFile):
+def readGenome(aFile):
     ''' Funtion to read the fastq file '''
     f = open(aFile)
     aval = ''.join([i.rstrip() for i in f.readlines() if not i[0] == '>'])
-    return aval
-
-genome = fileRead(genome_info)
-asd = genome[:500]
+    return aval.upper()
 
 def countFrequency(listToPass):
-    ''' This functions counts the frequency of each base present in out genome '''
+    ''' This functions counts the frequency of each base present in our genome '''
     base_count = {'A':0,'G':0,'C':0,'T':0}
     for i in listToPass:
         if i in base_count.keys():
             base_count[i] += 1
     return base_count
+
+def readSequence(seq):
+    f = open(seq,'r')
+    sequence,quality = [],[]
+    while True:
+        f.readline()
+        seq = f.readline().rstrip()
+        f.readline()
+        qual = f.readline().rstrip()
+        if len(seq) == 0: break # Exit the while loop
+        sequence.append(seq)
+        quality.append(qual)
+    return sequence,quality
+
+genome = readGenome(genome_info)
+asd = genome[:500]
 print(asd)
 print(countFrequency(asd))
